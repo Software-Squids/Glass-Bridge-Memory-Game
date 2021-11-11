534627f36de10775a26cf6911185a7d947d17628
GabrielleHieken <gabriellehieken@gmail.com>
2021-11-11 17:44:30 -0600

    Help and Highscore/ basic layout

--- src/pages/Help/HelpPage.jsx
+++ src/pages/Help/HelpPage.jsx
@@ -3,24 +3,47 @@ import '../../App.css';
 import { SquidText } from '../../components';
 import styled from 'styled-components';
 
+import line from '../../images/pinkLine1.jpg'
 
 
+//center help
 const Center = styled.div`
 position: absolute;
-width: 623px;
-height: 79px;
-left: 408px;
-top: 72px;
-
+bottom: 78%;
 `;
-//const Line = styled.div``
 
+//lines 
+const Line1 = styled.div`
+position: relative;
+width: 639px;
+height: 0px;
+left: 360px;
+top: 0px;
+transform: rotate(90deg);
+`
+
+//block of text 
+const Description = styled.div`
+position: absolute;
+color: #FFFFFF;
+font-size: 12px;
+padding 300px;
+`;
 
 function HelpPage() {
     return (
-        <Center>
-            <h1><SquidText> Help </SquidText></h1>
-        </Center>
-    );
+        <>  <Center>
+                <h1><SquidText> Help </SquidText></h1>
+                <Line1>
+                    <img src={line} alt="lines"/>
+                </Line1>
+            </Center>
+            <Description>
+                <h1>When you press "New Game" from the start menu, you will see two lines of boxes. A path will appear for you to blah blah blah blagh blah yadda yadda yadda yadda  </h1>
+            </Description>
+            
+        </>
+        
+    )
 }
 export default HelpPage;
--- src/pages/HighScores/HighScoresPage.jsx
+++ src/pages/HighScores/HighScoresPage.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import '../../App.css';
 import { SquidText } from '../../components';
-import styled from 'styled-components';
 
+import styled from 'styled-components';
+import line from '../../images/pinkLine1.jpg'
+
+//center help
 const Center = styled.div`
-    text-align: center; 
+position: absolute;
+bottom: 78%;
 `;
+
+//lines 
+const Line1 = styled.div`
+position: relative;
+width: 639px;
+height: 0px;
+left: 360px;
+top: 0px;
+transform: rotate(90deg);
+`
 
 function HighScoresPage() {
     return (
+        <>
         <Center>
         <h1><SquidText> high scores </SquidText></h1>
+        <Line1>
+                    <img src={line} alt="lines"/>
+        </Line1>
         </Center>
+        </>
     );
 }
 export default HighScoresPage;
